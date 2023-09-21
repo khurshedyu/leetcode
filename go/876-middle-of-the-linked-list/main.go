8534dc2bb530febe42224594e989e076a68f23e4
Khurshed Yusufov <khurshedyu@gmail.com>
2023-09-21 14:39:25 +0500

    upd README.md

--- go/876-middle-of-the-linked-list/main.go
+++ go/876-middle-of-the-linked-list/main.go
@@ -1,31 +1,8 @@
 package main
 
-import "fmt"
-
 type ListNode struct {
 	Val  int
 	Next *ListNode
-}
-
-func main() {
-	head := &ListNode{
-		Val: 1,
-		Next: &ListNode{
-			Val: 2,
-			Next: &ListNode{
-				Val: 3,
-				Next: &ListNode{
-					Val: 4,
-					Next: &ListNode{
-						Val:  5,
-						Next: nil,
-					},
-				},
-			},
-		},
-	}
-
-	fmt.Println(middleNode(head))
 }
 
 func middleNode(head *ListNode) *ListNode {
